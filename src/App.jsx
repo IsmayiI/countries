e574f25618f93415d6c4a2cadcf71a633727f597
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-04-30 14:13:53 +0400

    add assets

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
    return (
       <>
-
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus optio molestias sunt cupiditate, vel eveniet porro odit? Quis sed asperiores similique provident esse consequuntur reiciendis sunt, excepturi ut, dolore ex!</p>
       </>
    )
 }
