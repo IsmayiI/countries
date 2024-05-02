d1c05d1c34bee012d2c312b2f659cc1422f32ce6
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-05-02 14:02:57 +0400

    create Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
    return (
       <>
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus optio molestias sunt cupiditate, vel eveniet porro odit? Quis sed asperiores similique provident esse consequuntur reiciendis sunt, excepturi ut, dolore ex!</p>
+         <Header />
       </>
    )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,61 @@
+import { IoMoon, IoMoonOutline } from "react-icons/io5";
+import styled from "styled-components";
+import { Container } from "./Container";
+import { useEffect, useState } from "react";
+
+const HeaderEl = styled.header`
+   box-shadow: var(--shadow);
+   background-color: var(--colors-ui-base);
+`
+
+const Wrapper = styled.div`
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   padding: 2rem 0;
+`
+
+const Title = styled.a.attrs({
+   href: '/'
+})`
+   color: var(--colors-text);
+   font-size: var(--fs-sm);
+   font-weight: var(--fw-bold);
+   text-decoration: none;
+`
+
+const ModeSwitcher = styled.div`
+   color: var(--colors-text);
+   font-size: var(--fs-sm);
+   // font-weight: var(--fw-bold);
+   text-transform: capitalize;
+   cursor: pointer;
+`
+
+const Header = () => {
+   const [theme, setTheme] = useState('light')
+
+   const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
+
+   useEffect(() => {
+      document.body.setAttribute('data-theme', theme)
+   }, [theme])
+
+   const themeIcon = theme === 'light' ? <IoMoonOutline size="14px" /> : <IoMoon size="14px" />
+
+   return (
+      <HeaderEl>
+         <Container>
+            <Wrapper>
+               <Title>Where is the world?</Title>
+               <ModeSwitcher onClick={toggleTheme}>
+                  {themeIcon}
+                  <span style={{ marginLeft: '0.75rem' }}>{theme} Theme</span>
+               </ModeSwitcher>
+            </Wrapper>
+         </Container>
+      </HeaderEl>
+   )
+}
+
+export default Header
