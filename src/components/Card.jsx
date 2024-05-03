5295821153b72d3f911c4d637111d8d1f8d16f60
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-05-03 14:15:17 +0400

    add style to Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,49 @@
 import styled from "styled-components"
 
-const Wrapper = styled.article``
+const Wrapper = styled.article`
+   border-radius: var(--radii);
+   background-color: var(--colors-ui-base);
+   box-shadow: var(--shadow);
+   cursor: pointer;
+   overflow: hidden;
+`
 
-const CardImg = styled.img``
+const CardImg = styled.img`
+   display: block;
+   width: 100%;
+   height: 150px;
+   object-fit: cover;
+   object-position: center;
+   box-shadow: var(--shadow);
+`
 
-const CardBody = styled.div``
+const CardBody = styled.div`
+   padding: 1rem 1.5rem 2rem;
+`
 
-const CardTitle = styled.h3``
+const CardTitle = styled.h3`
+   margin: 0;
+   font-size: var(--fs-md);
+   font-weight: var(--fw-bold);
+`
 
-const CardList = styled.ul``
+const CardList = styled.ul`
+   list-style: none;
+   margin: 0;
+   padding: 1rem 0 0;
+`
 
-const CardListItem = styled.li``
+const CardListItem = styled.li`
+   display: flex;
+   justify-content: space-between;
+   font-size: var(--fs-sm);
+   font-weight: var(--fw-light);
+   line-height: 1.5;
+
+   & > b {
+      font-weight: var(--fw-bold);   
+   }
+`
 
 
 const Card = ({ img, name, info = [], onClick }) => {
