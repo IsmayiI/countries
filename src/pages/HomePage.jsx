9f89ab2163d0e192ae60e4a8943ffc27f2844044
Ismayil Ismayilov <ismayilov_ismayil_97@mail.ru>
2024-05-07 00:20:27 +0400

    create filtering functionality

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,17 +9,37 @@ import { ALL_COUNTRIES } from "../config"
 
 const HomePage = () => {
    const [countries, setCountries] = useState([])
+   const [filteredCountries, setFilteredCountries] = useState([])
+
+   const searchHandler = (search, region) => {
+      setFilteredCountries(null)
+      let data = [...countries]
+
+      if (region) {
+         data = data.filter(c => c.region.includes(region))
+      }
+
+      if (search) {
+         data = data.filter(c => c.name.common.toLowerCase().includes(search.toLowerCase()))
+      }
+
+      setFilteredCountries(data)
+   }
 
 
    useEffect(() => {
-      axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data))
+      axios.get(ALL_COUNTRIES).then(({ data }) => {
+         setCountries(data)
+         setFilteredCountries(data)
+      })
    }, [])
+
 
    return (
       <>
-         <Controls />
+         <Controls onSearch={searchHandler} />
          <List>
-            {countries.map(c => {
+            {filteredCountries.map(c => {
                const countryInfo = {
                   img: c.flags.png,
                   name: c.name.common,
